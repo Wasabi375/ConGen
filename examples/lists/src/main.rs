cfdf0be1b3417934416ccd5c181d5c49f2411611
Burkhard Mittelbach <wasabi37a@googlemail.com>
2026-05-13 15:53:45 +0200

    Add test cases for List of simple fields

--- examples/lists/src/main.rs
+++ examples/lists/src/main.rs
@@ -66,3 +66,92 @@ mod cli_test {
         Config(CongenClap<Config>),
     }
 }
+
+#[cfg(test)]
+mod test {
+    use clap::{Command, FromArgMatches};
+    use congen::{Configuration, CongenClap};
+
+    use crate::{Config, SubConfig};
+
+    fn test_command() -> Command {
+        CongenClap::<Config>::create_cmd("config")
+    }
+
+    fn base_config() -> Config {
+        Config {
+            field: 67,
+            comp: SubConfig { a: 42, b: 69 },
+            field_list: vec![1, 2, 3],
+        }
+    }
+
+    fn apply_change(config: &mut Config, args: &[&str]) {
+        let matches = test_command().get_matches_from(args);
+        let change = CongenClap::<Config>::from_arg_matches(&matches)
+            .expect("parse command")
+            .into_change();
+        config.apply_change(change);
+    }
+
+    #[test]
+    fn config_field_list_append() {
+        let mut config = base_config();
+        apply_change(&mut config, &["config", "field_list", "append", "10"]);
+        assert_eq!(config.field_list, vec![1, 2, 3, 10]);
+    }
+
+    #[test]
+    fn config_field_list_update_valid_key() {
+        let mut config = base_config();
+        apply_change(&mut config, &["config", "field_list", "update", "1", "10"]);
+        assert_eq!(config.field_list, vec![1, 10, 3]);
+    }
+
+    #[test]
+    #[should_panic(expected = "User specified index")]
+    fn config_field_list_update_out_of_range_key_panics() {
+        let mut config = base_config();
+        apply_change(
+            &mut config,
+            &["config", "field_list", "update", "99", "10"],
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "User specified index")]
+    fn config_field_list_update_empty_list_key_zero_panics() {
+        let mut config = base_config();
+        config.field_list.clear();
+        apply_change(&mut config, &["config", "field_list", "update", "0", "10"]);
+    }
+
+    #[test]
+    fn config_field_list_remove_valid_key() {
+        let mut config = base_config();
+        apply_change(&mut config, &["config", "field_list", "remove", "1"]);
+        assert_eq!(config.field_list, vec![1, 3]);
+    }
+
+    #[test]
+    #[should_panic(expected = "User specified index")]
+    fn config_field_list_remove_out_of_range_key_panics() {
+        let mut config = base_config();
+        apply_change(&mut config, &["config", "field_list", "remove", "99"]);
+    }
+
+    #[test]
+    #[should_panic(expected = "User specified index")]
+    fn config_field_list_remove_empty_list_key_zero_panics() {
+        let mut config = base_config();
+        config.field_list.clear();
+        apply_change(&mut config, &["config", "field_list", "remove", "0"]);
+    }
+
+    #[test]
+    fn config_field_list_empty() {
+        let mut config = base_config();
+        apply_change(&mut config, &["config", "field_list", "empty"]);
+        assert!(config.field_list.is_empty());
+    }
+}
